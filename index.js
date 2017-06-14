3a735d9d05ea94a06d389fdf101dd907476923ec
Frances Thai <fthai@fthai-macbookpro1.roam.corp.google.com>
2017-06-14 08:38:45 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+var bodyParser = require('body-parser')
+
+//Faye Stuff (websocket)
+var faye = require('faye')
+var http = require('http')
+var faye_server = new faye.NodeAdapter({mount: '/faye', timeout: 45});
+console.log('Firing up faye server...')
+// Handle non-Bayeux requests
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello, non-faye request');
+});
+
+faye_server.attach(server);
+server.listen(8089);
+
+const app = express()
+
+ 
+// parse application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json 
+app.use(bodyParser.json())
+app.use(express.static(__dirname + '/public'));
+//Store all HTML files in view folder.
+app.use(express.static(__dirname + '/assets'));
+//Store all JS and CSS in Scripts folder.
+app.get('/',function(req,res){
+  res.sendFile('/index.html');
+})
+app.post('/new-comment', function (req, res) {
+  console.log('Adding new comment')
+  console.log(req.body)
+  faye_server.getClient().publish('/new-comment', {comment: req.body.comment})
+})
+app.post('/play-video', function(req, res) {
+	console.log('Attempting to play video...')
+	faye_server.getClient().publish('/play-video', {videoID: req.body.videoID})
+})
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
